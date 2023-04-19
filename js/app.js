beec48c6906288c37eb21cf48812578c52f59615
Vianne Loro <95766596+viannejohn@users.noreply.github.com>
2023-04-19 21:18:02 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[title="Go to Homepage"]').tooltip({placement: "bottom"});
+    $('[title="Go to Contact Us Page"]').tooltip();
+    $('[title="Go to About Us Page"]').tooltip();
+    $('[title="OK Computer"]').tooltip();
+})
